92cc1fff784aedd0f5ef037f72e54734480e4840
abstract-animation-9914 <vm930@hotmail.com>
2019-02-10 02:37:06 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,7 +4,7 @@ WIN_COMBINATIONS = [
   [0,1,2],#top row 
   [3,4,5],#middle row 
   [6,7,8],#bottom row 
-  [0,3,6], #1st vertical
+  [0,3,6],#1st vertical
   [1,4,7],#vertical
   [2,5,8],#vertical
   [0,4,8],#diagonals
