e6ae940353742d5576087da11baf91ef2cc240ae
abstract-animation-9914 <vm930@hotmail.com>
2019-02-10 03:07:25 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,7 @@ WIN_COMBINATIONS = [
 
 def won(board)
   WIN_COMBINATIONS.find do 
-    |combo| board[combo[0]] == board[combo[1]]&& board[combo[1]] == board[combo[2]] && position_taken?()
+    |combo| board[combo[0]] == board[combo[1]]&& board[combo[1]] == board[combo[2]] && position_taken?(board,combo[])
     
     end 
 end 
