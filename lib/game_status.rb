d99e06aba9906f8b6ec0d9fa5bfc02f7c154b50b
abstract-animation-9914 <vm930@hotmail.com>
2019-02-10 02:14:45 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -12,12 +12,16 @@ WIN_COMBINATIONS = [
   ]
 
 def won(board) #should return false/nil if theres no win combo
+  
   for each win_combination in WIN_COMBINATIONS
   win_index_1 = WIN_COMBINATIONS[0]
   win_index_2 = WIN_COMBINATIONS[1]
   win_index_3 = WIN_COMBINATIONS[2]
   
   postion_1 = board[win_index_1]
+  postion_2 = board[win_index_2]
+  postion_3 = board[win_index_3]
+  
 end 
 
 def position_taken?(board, index)
